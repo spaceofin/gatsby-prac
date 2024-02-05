9d5783f518086d83d7754132b73cc2aaf713bd83
spaceofin <ineemig@gmail.com>
2024-02-06 01:13:52 +0900

    update: practice page query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,42 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+// const BlogPage = () => {
+//     return (
+//         <Layout pageTitle="My Blog Posts">
+//             <p>My cool posts will go in here</p>
+//         </Layout>
+//     )
+// }
+
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go in here</p>
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
+
+export const ff = graphql`
+  query {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
 
 export const Head = () => <Seo title="My Blog Posts" />
 
